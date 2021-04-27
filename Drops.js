e628437ad7fd0c2fca8907bd1b680a37ec56c751
Lakshya2510 <74653849+Lakshya2510@users.noreply.github.com>
2021-04-27 14:34:37 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,27 @@
+class Drops {
+    constructor(x,y){
+        var options = {
+            isStatic: false,
+            friction: 0.1
+        }
+        this.rain = Bodies.circle(x,y,5,options);
+        this.radius = 5;
+        World.add(world, this.rain)
+        //load Image for BestMan
+        
+    }
+
+    updateY(){
+        if(this.rain.position.y>height){
+            Matter.Body.setPosition(this.rain,{x:random(0,400),y:random(0,400)})
+        }
+    }
+
+    showDrops()  {
+        fill("Blue")
+        ellipseMode(CENTER)
+        ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius)
+    }
+}
+
+
